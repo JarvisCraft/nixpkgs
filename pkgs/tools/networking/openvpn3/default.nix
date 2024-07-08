3a93eea39ad6897699209347c1220d32eb2ab5e8
Petr Portnov <mrjarviscraft+nix@gmail.com>
2024-07-08 20:56:36 +0300

    openvpn3: 20 -> 22_dev

--- pkgs/tools/networking/openvpn3/default.nix
+++ pkgs/tools/networking/openvpn3/default.nix
@@ -2,10 +2,7 @@
 , stdenv
 , fetchFromGitHub
 , asio
-, autoconf-archive
-, autoreconfHook
 , glib
-, gtest
 , jsoncpp
 , libcap_ng
 , libnl
@@ -18,30 +15,42 @@
 , systemd
 , enableSystemdResolved ? false
 , tinyxml-2
-, wrapGAppsHook3
+, wrapGAppsHook3 # TODO: do we need it?
+# begin(pportnov)
+, meson
+, ninja
+, bash
+, breakpointHook
+, gdbuspp
+, cmake
+, git
+# end(pportnov)
 }:
 
-let
-  openvpn3-core = fetchFromGitHub {
-    owner = "OpenVPN";
-    repo = "openvpn3";
-    rev = "7590cb109349809b948e8edaeecabdbfe24e4b17";
-    hash = "sha256-S9D/FQa7HYj0FJnyb5dCrtgTH9Nf2nvtyp/VHiebq7I=";
-  };
-in
 stdenv.mkDerivation rec {
   pname = "openvpn3";
   # also update openvpn3-core
-  version = "20";
+  version = "22_dev";
 
   src = fetchFromGitHub {
     owner = "OpenVPN";
+    #owner = "JarvisCraft";
     repo = "openvpn3-linux";
-    rev = "v${version}";
-    hash = "sha256-Weyb+rcx04mpDdcL7Qt4O+PvPf5MLPAP/Uy+8qoNXbQ=";
+    #rev = "v${version}-nixos";
+    rev = "62f3536b015ab9f348a2a87e32864cc330709c1a";
+    hash = "sha256-ol2WMkfe8yrK/ICQ2gyDEDwqmpMXWvGRrWANg1uJ03Q=";
+    fetchSubmodules = true;
+    # This is required to generate version information.
+    leaveDotGit = true;
   };
 
-  postPatch = ''
+  patches = [
+    #./0001-fix-use-usr-bin-env-bash-in-scripts.patch
+    #./0002-build-shorter-asio-path.patch
+    ./0003-no-auto-install.patch
+  ];
+
+  /*postPatch = ''
     rm -r ./vendor/googletest
     cp -r ${gtest.src} ./vendor/googletest
     rm -r ./openvpn3-core
@@ -50,20 +59,25 @@ stdenv.mkDerivation rec {
     chmod -R +w ./vendor/googletest
     shopt -s globstar
 
-    patchShebangs **/*.py **/*.sh ./src/python/{openvpn2,openvpn3-as,openvpn3-autoload} \
+    patchShebangs ** /*.py ** /*.sh ./src/python/{openvpn2,openvpn3-as,openvpn3-autoload} \
     ./distro/systemd/openvpn3-systemd ./src/tests/dbus/netcfg-subscription-test
 
     echo "3.git:v${version}:unknown" > openvpn3-core-version
-  '';
+  '';*/
 
-  preAutoreconf = ''
-    substituteInPlace ./update-version-m4.sh --replace 'VERSION="$(git describe --always --tags)"' "VERSION=v${version}"
-    ./update-version-m4.sh
-  '';
+  # TODO(pportnov): do we need it?
+  outputs = [ "out" "dev" ];
 
   nativeBuildInputs = [
-    autoconf-archive
-    autoreconfHook
+    # begin(pportnov)
+    meson
+    ninja
+    bash
+    cmake
+    # breakpointHook
+    git
+    # end(pportnov)
+
     python3.pkgs.docutils
     python3.pkgs.jinja2
     pkg-config
@@ -82,9 +96,26 @@ stdenv.mkDerivation rec {
     openssl
     protobuf
     tinyxml-2
+    gdbuspp
   ] ++ lib.optionals enableSystemdResolved [
     systemd
   ];
+
+  mesonFlags = [
+    (lib.mesonOption "selinux" "disabled")
+    (lib.mesonOption "selinux_policy" "disabled")
+    # "-Dbash-completion=enabled"
+    (lib.mesonOption "test_programs" "disabled")
+    (lib.mesonOption "unit_tests" "disabled")
+    (lib.mesonOption "asio_path" "${asio}")
+    # "openvpn3-linux-${version}"
+    # "-Ddbus-1:datadir=$out/share/"
+    (lib.mesonOption "dbuspolicydir" "${placeholder "out"}/share/dbus-1/system.d")
+    # (lib.mesonOption "dbussessionservicedir" "${placeholder "out"}/share/dbus-1/services")
+    (lib.mesonOption "dbussystemservicedir" "${placeholder "out"}/share/dbus-1/system-services")
+  ];
+
+  # mesonWrapMode = "default";
 
   # runtime deps
   pythonPath = with python3.pkgs; [
@@ -100,6 +131,7 @@ stdenv.mkDerivation rec {
     wrapPythonPrograms
   '';
 
+  /* `mesonFlags` are used instead
   configureFlags = [
     "--enable-bash-completion"
     "--enable-addons-aws"
@@ -110,14 +142,28 @@ stdenv.mkDerivation rec {
     # https://github.com/OpenVPN/openvpn3-linux/blob/v20/configure.ac#L434
     "DEFAULT_DNS_RESOLVER=--systemd-resolved"
   ];
+  */
+
+  postInstall = ''
+    for i in $out/share/dbus-1/system-services/*.service; do
+      # substituteInPlace $i --replace /bin/false $coreutils/bin/false
+      echo "===== File $i ====="
+      cat $i
+      echo "===== ======= ====="
+    done
+  '';
 
   NIX_LDFLAGS = "-lpthread";
 
-  meta = with lib; {
+  meta = {
     description = "OpenVPN 3 Linux client";
-    license = licenses.agpl3Plus;
+    license = lib.licenses.agpl3Plus;
     homepage = "https://github.com/OpenVPN/openvpn3-linux/";
-    maintainers = with maintainers; [ shamilton kfears ];
-    platforms = platforms.linux;
+    maintainers = [
+      lib.maintainers.shamilton
+      lib.maintainers.kfears
+      lib.maintainers.progrm_jarvis
+    ];
+    platforms = lib.platforms.linux;
   };
 }
